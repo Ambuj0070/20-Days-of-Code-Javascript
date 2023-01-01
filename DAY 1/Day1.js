d6f5d234921c6caa904e6b59dc28620ea7586a6c
Ambuj Singh <114945245+Ambuj0070@users.noreply.github.com>
2023-01-01 23:49:35 +0530

    Add files via upload

---
+++ DAY 1/Day1.js
@@ -0,0 +1,18 @@
+let firstName = 'Ambuj'    
+let lastName = 'Singh'     
+let country = 'India'      
+let city = 'Kanpur'         
+let age = 20
+let isMarried = false
+
+const gravity = 9.81         
+const boilingPoint = 100   
+const PI = 3.14          
+
+let name = 'Ambuj',     
+  job = 'Student',
+  live = 'India'
+
+console.log(firstName, lastName, country, city, age, isMarried) 
+console.log(gravity, boilingPoint, PI) 
+console.log(name, job, live)
